c827d1044ab6c719354f13940c76f9744c7c937a
Alvin <Alvin@DESKTOP-QGSA128>
2017-08-05 11:19:08 +0800

    delete original solution

--- DataAnalystics/DataAnalystics/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace DataAnalystics.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
