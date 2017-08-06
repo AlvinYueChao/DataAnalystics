41637e26150ea853d20b59a12232ecdbe241abe1
Alvin <Alvin@DESKTOP-QGSA128>
2017-08-06 19:11:37 +0800

    add candlestick chart with static data.

---
+++ DataAnalysis/DataAnalysis/Controllers/CandlestickController.cs
@@ -0,0 +1,27 @@
+﻿using DataAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace DataAnalysis.Controllers
+{
+    public class CandlestickController : Controller
+    {
+        private List<Stock> result = new List<Stock>();
+        private StockHandler sh = new StockHandler();
+        // GET: Candlestck
+        public ActionResult Index(String symbol, int date, int start, int end)
+        {
+            result = sh.GetStocksByParams(symbol, date, start, end).ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetCharts()
+        {
+            return Json(new { count = result.Count, data = result}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
