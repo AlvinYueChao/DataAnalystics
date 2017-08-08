cb1e51d0d37fd8293f9d2e5f77fd9be6bf1ae554
mohanyi <evamore121@hotmail.com>
2017-08-08 13:48:10 +0800

    remove unused button

--- DataAnalysis/DataAnalysis/Controllers/StocksController.cs
+++ DataAnalysis/DataAnalysis/Controllers/StocksController.cs
@@ -16,11 +16,6 @@ namespace DataAnalysis.Controllers
         // GET: Stocks
         public ActionResult Index()
         {
-            //IList<Stock> allstocks = db.Stocks.ToList();
-            //var result = (from s in allstocks
-            //              where s.Symbol == symbol && s.Date == date
-            //              select s).ToList();
-            //return View(result);
             return View();
         }
 
@@ -31,109 +26,5 @@ namespace DataAnalysis.Controllers
             IList<Stock> result = sh.GetStocksByFourParams(symbol, date, start, end);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-
-        //// GET: Stocks/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Stock stock = db.Stocks.Find(id);
-        //    if (stock == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(stock);
-        //}
-
-        //// GET: Stocks/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Stocks/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "ID,Symbol,Date,Time,Open,High,Low,Close,Volume,SplitFactor")] Stock stock)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Stocks.Add(stock);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(stock);
-        //}
-
-        //// GET: Stocks/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Stock stock = db.Stocks.Find(id);
-        //    if (stock == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(stock);
-        //}
-
-        //// POST: Stocks/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "ID,Symbol,Date,Time,Open,High,Low,Close,Volume,SplitFactor")] Stock stock)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(stock).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(stock);
-        //}
-
-        //// GET: Stocks/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Stock stock = db.Stocks.Find(id);
-        //    if (stock == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(stock);
-        //}
-
-        //// POST: Stocks/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    Stock stock = db.Stocks.Find(id);
-        //    db.Stocks.Remove(stock);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
     }
 }
